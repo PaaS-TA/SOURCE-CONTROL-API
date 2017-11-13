0afa85c5689ff6c0f3d86dfbe2e20338b8575b42
ijlee <214304>
2017-11-13 14:05:26 +0900

    테스트 케이스 작성

--- src/test/java/com/paasta/scapi/service/ScLoginServiceTest.java
+++ src/test/java/com/paasta/scapi/service/ScLoginServiceTest.java
@@ -1,48 +1,54 @@
 package com.paasta.scapi.service;
 
+import com.paasta.scapi.common.util.PropertiesUtil;
+import com.sun.jersey.api.client.Client;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import sonia.scm.user.User;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
-
+import com.paasta.scapi.common.ClientTestUtil;
 /**
  * Created by ijlee on 2017-09-04.
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class, classes = { PropertiesUtil.class })
+@ActiveProfiles("test")
+public class ScLoginServiceTest extends ScLoginService{
 
-public class ScLoginServiceTest {
-
-    @InjectMocks
+//    private MockMvc mockMvc;
     private ScLoginService scLoginService;
 
-    @Value("${admin.id}") String admin_id;
+    @Override
+    public ResponseEntity login(User user, PropertiesUtil propertiesUtil) {
+        ClientTestUtil.createAdminSession();
+        return new ResponseEntity(user,HttpStatus.OK);
+    }
+    @Autowired
+    private PropertiesUtil propertiesUtil;
+
     @Before
-    public void setup() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
+        scLoginService = new ScLoginService();
     }
-
-    @Test
-    public void login_500() throws Exception {
-        User user = mock(User.class);
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,scLoginService.login(user).getStatusCode());
-    }
-
     @Test
     public void login_200() throws Exception {
-
-        User user = new User("a");
-        user.setPassword("a");
-
-        assertNotNull(scLoginService.login(user).getStatusCode());
+        User user = mock(User.class);
+        assertEquals(HttpStatus.OK,login(user, propertiesUtil).getStatusCode());
     }
 
 }
