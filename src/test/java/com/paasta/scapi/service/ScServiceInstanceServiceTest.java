93c274dd104c06e754ffe8f87e278e9cbdbed165
ijlee <214304>
2017-11-20 09:41:09 +0900

    coverage 로 인한 static service 분리작업

--- src/test/java/com/paasta/scapi/service/ScServiceInstanceServiceTest.java
+++ src/test/java/com/paasta/scapi/service/ScServiceInstanceServiceTest.java
@@ -1,35 +1,31 @@
 package com.paasta.scapi.service;
 
 import com.paasta.scapi.entity.ScServiceInstance;
-import com.paasta.scapi.repository.ScServiceInstanceRepository;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.runners.MethodSorters;
 import org.springframework.data.domain.Page;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
-@RunWith(MockitoJUnitRunner.class)
-public class ScServiceInstanceServiceTest {
-
-    /**
-     * SCM Server connection Session Information
-     */
-
-    @Mock
-    ScServiceInstanceRepository scServiceInstanceRepository;
-
-    @Mock
-    ScServiceInstanceService scServiceInstanceService;
+/**
+ * create ScServiceInstanceServiceTest
+ * @author ijlee
+ * @since 1.0
+ * lee on 2017-08-02.
+ */
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ScServiceInstanceServiceTest  extends CommonServiceTest{
 
     @Before
-    public void setup() throws Exception {
-        MockitoAnnotations.initMocks(this);
+    public void setUp() {
+        setUpMockUtil();
     }
 
     @Test
@@ -39,7 +35,7 @@ public class ScServiceInstanceServiceTest {
     }
 
     @Test
-    public void getServiceInstanceList() throws Exception {
+    public void getServiceInstanceListC() throws Exception {
         //String organizationName, Pageable pageable
         Page<ScServiceInstance> serviceInstanceListPage = mock(Page.class);
         scServiceInstanceService.getServiceInstanceList("");
