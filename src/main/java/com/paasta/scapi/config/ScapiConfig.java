1db151159218bfb3a8291d405d7282225de3da47
jinhyojin <hjinjin@crossent.com>
2021-12-13 14:45:06 +0900

    Update CVE : HTTP TRACE Method disabled

--- src/main/java/com/paasta/scapi/config/ScapiConfig.java
+++ src/main/java/com/paasta/scapi/config/ScapiConfig.java
@@ -1,6 +1,9 @@
 package com.paasta.scapi.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,5 +16,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.paasta.scapi.entity")
 @ComponentScan(basePackages = "com.paasta")
 public class ScapiConfig {
+
+    @Bean
+    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+
+        factory.addConnectorCustomizers(connector -> {
+            connector.setAllowTrace(true);
+        });
+        return factory;
+    }
 }
 
